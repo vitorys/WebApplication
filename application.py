ca41819f191950c137fd750670dfda52f689c7cc
Vitor Yudi Shinohara <vitor.ysh@gmail.com>
2018-03-26 17:27:34 -0300

    Computer's list implemented

--- application.py
+++ application.py
@@ -7,14 +7,25 @@ import dill
 
 render = web.template.render('templates/')
 
-urls = ('/', 'index')
+urls = ('/', 'index','/gpu_usage', 'gpu')
 app = web.application(urls, globals())
-
 myform = form.Form( 
-	form.Textbox("TrainCharacteristcs"), 
+	form.Textbox("TrainCharacteristics"), 
 	form.Textbox("TrainLabels"),
 	form.Textbox("gamma"),
 	form.Textbox("c"))
+
+class gpu:
+	def GET(self):
+		return self.verifyGPUUsage()
+
+	def verifyGPUUsage(self):
+		# A ser implementado
+
+		# Retorna 1 se a máquina estiver ociosa
+		# Retorna 0 se a máquina estiver sendo utilizada
+		return 1
+
 
 class index: 
 	def GET(self): 
@@ -22,27 +33,30 @@ class index:
 		return render.formtest(form)
 
 	def POST(self): 
-		form = myform() 
-		if not form.validates(): 
-			return render.formtest(form)
+		form = myform()
+
+		if not form.validates():
+			return render.register(f)
+
 		else:
-			train_characteristics = eval(form['TrainCharacteristcs'].value)
+			train_characteristics = eval(form['TrainCharacteristics'].value)
 			train_labels = eval(form['TrainLabels'].value)
 			
 			gamma = float(form['gamma'].value)
 			c = float(form['c'].value)
 			
 			classifier = Classifier()
-			#return 'Olá mundo'
-			return classifier.svm_classifier(train_characteristics, train_labels, gamma, c)
+			f_ = classifier.svm_classifier(train_characteristics, train_labels, gamma, c)
+			return f_
 			
 class Classifier:
 	def svm_classifier(self, train_characteristics, train_labels, gamma, c):
 		
 		clf = svm.SVC(gamma=gamma, C=c, kernel='rbf')
 		clf.fit(train_characteristics, train_labels)
-		
-		return dill.dumps(clf)
+		f_ = dill.dumps(clf)
+		print type(f_)
+		return f_
 
 if __name__=="__main__":
 	web.internalerror = web.debugerror
---
+++ computers.py
@@ -0,0 +1,6 @@
+computer = {
+	'Computer1' : 'http://localhost:8080/',
+	'Computer2' : 'http://localhost:8080/',
+	'Computer3' : 'http://localhost:8080/',
+	'Computer4' : 'http://localhost:8080/'
+}
