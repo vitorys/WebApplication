93c452ffc29e004470f455cfa40940f7c06c6a20
Vitor Yudi Shinohara <vitor.ysh@gmail.com>
2018-04-18 12:25:07 -0300

    Temporary models implemented at server side

--- application.py
+++ application.py
@@ -2,38 +2,71 @@
 
 import web
 from web import form
-from sklearn import svm
 import dill
+import tempfile
+from Classifier import Classifier
 
 render = web.template.render('templates/')
 
-urls = ('/', 'index','/gpu_usage', 'gpu')
+urls = ('/', 'Index','/gpu_usage', 'Gpu', '/train', 'Train')
 app = web.application(urls, globals())
-myform = form.Form( 
+
+indexForm = form.Form( 
 	form.Textbox("TrainCharacteristics"), 
 	form.Textbox("TrainLabels"),
 	form.Textbox("gamma"),
 	form.Textbox("c"))
 
-class gpu:
+
+predictForm = form.Form( 
+	form.Textbox("TestCharacteristics"),
+	form.Textbox("Model_ID"))
+
+
+class Train:
 	def GET(self):
-		return self.verifyGPUUsage()
-
-	def verifyGPUUsage(self):
-		# A ser implementado
-
-		# Retorna 1 se a máquina estiver ociosa
-		# Retorna 0 se a máquina estiver sendo utilizada
-		return 1
-
-
-class index: 
-	def GET(self): 
-		form = myform()
+		form = predictForm()
 		return render.formtest(form)
 
+
+	def POST(self):
+		form = predictForm()
+
+		if not form.validates():
+			return render.register(f)
+
+		else:
+			test_characteristics = eval(form['TestCharacteristics'].value)
+			Model_ID = str(form['Model_ID'].value)
+
+			model_path = './models/' + Model_ID
+			print model_path
+			
+			tmp_model = open(model_path, 'r')
+			content_tmp_model = tmp_model.read()
+			
+			model = dill.loads(content_tmp_model)
+			return model
+
+			classifier = Classifier()
+
+			# Realiza o predict e retorna o resultado
+			return classifier.predict(model, test_characteristics)
+
+
+class Gpu:
+	def GET(self):
+		return 1
+
+class Index:
+		
+	def GET(self): 
+		form = indexForm()
+		return render.formtest(form)
+
+	# Realiza o treinamento e armazena o modelo em uma lista -> Retorna o ID da máquina
 	def POST(self): 
-		form = myform()
+		form = indexForm()
 
 		if not form.validates():
 			return render.register(f)
@@ -47,17 +80,15 @@ class index:
 			
 			classifier = Classifier()
 			f_ = classifier.svm_classifier(train_characteristics, train_labels, gamma, c)
-			return f_
 			
-class Classifier:
-	def svm_classifier(self, train_characteristics, train_labels, gamma, c):
-		
-		clf = svm.SVC(gamma=gamma, C=c, kernel='rbf')
-		clf.fit(train_characteristics, train_labels)
-		f_ = dill.dumps(clf)
-		print type(f_)
-		return f_
+			tmp_file = tempfile.NamedTemporaryFile(dir='./models/', delete=False)
+			tmp_file.write( str(f_) )
+			tmp_file.close()
+			# Separação do caminho absoluto por /
+			file_name = tmp_file.name.split('/')
+			# Retorna somente o nome
+			return file_name[len(file_name)-1]
 
 if __name__=="__main__":
-	web.internalerror = web.debugerror
+	#web.internalerror = web.debugerror
 	app.run()
